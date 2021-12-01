784e0d983d73ea0a35f494364301cb3cac845d46
wangliangliang <2238632333@qq.com>
2021-12-01 17:29:25 +0800

    初始化测试数据

---
+++ test.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: WLL
+ * @Date: 2021-12-01 17:27:49
+ * @LastEditors: WLL
+ * @LastEditTime: 2021-12-01 17:28:58
+ * @Description: file content
+ */
+const a = 123;
+const b = 456;
+console.log(a + b);
